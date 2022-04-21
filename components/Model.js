0222496221bf9fda09267ee5bb3bffee85f34392
Swapnil Sharma <swapnilrsharma2806@gmail.com>
2022-04-21 13:12:16 +0530

    Initial commit

---
+++ components/Model.js
@@ -0,0 +1,19 @@
+import "@google/model-viewer";
+
+const Model = () => (
+  <div id="card">
+    <model-viewer
+      src="/Astronaut.glb"
+      ios-src=""
+      poster="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b%2Fposter-astronaut.png?v=1599079951717"
+      alt="A 3D model of an astronaut"
+      shadow-intensity="1"
+      camera-controls
+      auto-rotate
+      ar
+      ar-modes="webxr scene-viewer quick-look"
+    ></model-viewer>
+  </div>
+);
+
+export default Model;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import dynamic from "next/dynamic";
+
+const Model = dynamic(() => import("../components/Model"), { ssr: false });
+
+export default function Home() {
+  return <Model />;
+}
